2a0bab4b1fdf55342fbd2c10edcb8e1c5575abb3
Curtis Lussier <W0870569@myscc.ca>
2025-12-10 17:59:37 -0500

    Deploy Angular build

--- src/app/Data/ItemList.ts
+++ src/app/Data/ItemList.ts
@@ -4,10 +4,10 @@ export class ItemList extends Item
 {
   public $itemlist:Item[]=
     [
-      {$Id: 1, $ImagePath: "../artworks/Weather.png", $ImageName: "Wheather App", $Description: "Search the location and get all of the weather information you need", $dateCreated: "2023", $imageHeight: 335, $imageWidth: 300},
-      {$Id: 2, $ImagePath: "../artworks/Calc.png", $ImageName: "Calculator", $Description: "A simple python calculator with a gui", $dateCreated: "2023", $imageHeight: 335, $imageWidth: 350},
-      {$Id: 3, $ImagePath: "../artworks/Pong.png", $ImageName: "Pong", $Description: "A two player pong game that the player can play using pygame", $dateCreated: "2024", $imageHeight: 335, $imageWidth: 330},
-      {$Id: 4, $ImagePath: "../artworks/Undertale.png", $ImageName: "Undertale", $Description: "Re-Made undertale in javafx", $dateCreated: "2023", $imageHeight: 335, $imageWidth: 260},
+      {$Id: 1, $ImagePath: "../public/Weather.png", $ImageName: "Wheather App", $Description: "Search the location and get all of the weather information you need", $dateCreated: "2023", $imageHeight: 335, $imageWidth: 300},
+      {$Id: 2, $ImagePath: "../public/Calc.png", $ImageName: "Calculator", $Description: "A simple python calculator with a gui", $dateCreated: "2023", $imageHeight: 335, $imageWidth: 350},
+      {$Id: 3, $ImagePath: "../public/Pong.png", $ImageName: "Pong", $Description: "A two player pong game that the player can play using pygame", $dateCreated: "2024", $imageHeight: 335, $imageWidth: 330},
+      {$Id: 4, $ImagePath: "../public/Undertale.png", $ImageName: "Undertale", $Description: "Re-Made undertale in javafx", $dateCreated: "2023", $imageHeight: 335, $imageWidth: 260},
     ];
   getItems(): Item[]
   {
